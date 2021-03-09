730d73eb0225b2307de323b2dc386d55f90db52a
snowinszu <86755838@qq.com>
2021-03-09 20:19:23 +0800

    release v0.3.1

--- dist/sw.js
+++ dist/sw.js
@@ -2,4 +2,5 @@ self.importScripts('https://cdn.jsdelivr.net/npm/swarmcloud-sw@latest/dist/peer-
 var worker = new PeerWorker({
     // Other PeerWorker options provided by SwarmCloud
 });
+console.log(`PeerWorker version ${PeerWorker.version}`);
 worker.register();
